fe4fa92fb7f72d93122c428124944db2424c1ee4
Gokhan Kus <gkus1998@hotmail.com>
2024-05-23 15:58:19 +0300

    admin panel actors controller, cshtml refactored

--- TechCareer_BootCamp_MovieProject_UI/Areas/Admin/Controllers/ActorsController.cs
+++ TechCareer_BootCamp_MovieProject_UI/Areas/Admin/Controllers/ActorsController.cs
@@ -1,13 +1,101 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechCareer_BootCamp_MovieProject_Model.ViewModels.ActorModels;
+using TechCareer_BootCamp_MovieProject_Services.AbstractServices;
 
 namespace TechCareer_BootCamp_MovieProject_UI.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 	public class ActorsController : Controller
 	{
-		public IActionResult Index()
+		private readonly IServiceManager _manager;
+		public ActorsController(IServiceManager manager)
+		{
+			_manager = manager;
+		}
+		public IActionResult Create()
 		{
 			return View();
+		}
+		public async Task<IActionResult> Index()
+		{
+			var actors = await _manager.ActorService.GetAllActors(false);
+			return View(actors);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(ActorViewModelForInsertion actorViewModel, IFormFile? file)
+		{
+			if (ModelState.IsValid)
+			{
+				if (file != null)
+				{
+					string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MovieActors", file.FileName);
+					using (var stream = new FileStream(path, FileMode.Create))
+					{
+						await file.CopyToAsync(stream);
+					}
+					actorViewModel.ImagePath = file.FileName;
+				}
+				else
+				{
+					actorViewModel.ImagePath = "DefaultActor.jpg";
+				}
+				_manager.ActorService.CreateOneActor(actorViewModel);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(actorViewModel);
+		}
+		public IActionResult Edit(int id)
+		{
+			var actor = _manager.ActorService.GetOneActorForUpdate(id, false);
+			if (actor == null)
+			{
+				return NotFound();
+			}
+			return View(actor);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(ActorViewModelForUpdate actorViewModel, IFormFile? file)
+		{
+			if (ModelState.IsValid)
+			{
+				if (file is not null)
+				{
+					string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MovieActors", file.FileName);
+					using (var stream = new FileStream(path, FileMode.Create))
+					{
+						await file.CopyToAsync(stream);
+					}
+					actorViewModel.ImagePath = file.FileName;
+				}
+				_manager.ActorService.UpdateOneActor(actorViewModel);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(actorViewModel);
+		}
+		public IActionResult Delete(int id)
+		{
+			var actor = _manager.ActorService.GetOneActor(id, false);
+			if (actor == null)
+			{
+				return NotFound();
+			}
+
+			return View(actor);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var actor = _manager.ActorService.GetOneActor(id, false);
+			if (actor != null)
+			{
+				_manager.ActorService.DeleteOneActor(id);
+			}
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
--- TechCareer_BootCamp_MovieProject_UI/Controllers/ActorsController.cs
+++ TechCareer_BootCamp_MovieProject_UI/Controllers/ActorsController.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using TechCareer_BootCamp_MovieProject_Model.Entities;
-using TechCareer_BootCamp_MovieProject_Model.ViewModels.ActorModels;
-using TechCareer_BootCamp_MovieProject_Repositories.Context;
+﻿using Microsoft.AspNetCore.Mvc;
 using TechCareer_BootCamp_MovieProject_Services.AbstractServices;
 
 namespace TechCareer_BootCamp_MovieProject_UI.Controllers
@@ -15,18 +6,10 @@ namespace TechCareer_BootCamp_MovieProject_UI.Controllers
 	public class ActorsController : Controller
 	{
 		private readonly IServiceManager _manager;
-
 		public ActorsController(IServiceManager manager)
 		{
 			_manager = manager;
 		}
-
-		public async Task<IActionResult> Index()
-		{
-			var actors = await _manager.ActorService.GetAllActors(false);
-			return View(actors);
-		}
-
 		public async Task<IActionResult> Details(int id)
 		{
 			var actor = await _manager.ActorService.GetOneActorWithMovies(id, false);
@@ -35,87 +18,6 @@ namespace TechCareer_BootCamp_MovieProject_UI.Controllers
 				return NotFound();
 			}
 			return View(actor);
-		}
-
-		public IActionResult Create()
-		{
-			return View();
-		}
-
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create(ActorViewModelForInsertion actorViewModel, IFormFile? file)
-		{
-			if (ModelState.IsValid)
-			{
-				if (file != null)
-				{
-					string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MovieActors", file.FileName);
-					using (var stream = new FileStream(path, FileMode.Create))
-					{
-						await file.CopyToAsync(stream);
-					}
-					actorViewModel.ImagePath = file.FileName;
-				}
-				else
-				{
-					actorViewModel.ImagePath = "DefaultActor.jpg";
-				}
-				_manager.ActorService.CreateOneActor(actorViewModel);
-				return RedirectToAction(nameof(Index));
-			}
-			return View(actorViewModel);
-		}
-		public IActionResult Edit(int id)
-		{
-			var actor = _manager.ActorService.GetOneActorForUpdate(id, false);
-			if (actor == null)
-			{
-				return NotFound();
-			}
-			return View(actor);
-		}
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(ActorViewModelForUpdate actorViewModel, IFormFile? file)
-		{
-			if (ModelState.IsValid)
-			{
-				if (file is not null)
-				{
-					string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MovieActors", file.FileName);
-					using (var stream = new FileStream(path, FileMode.Create))
-					{
-						await file.CopyToAsync(stream);
-					}
-					actorViewModel.ImagePath = file.FileName;
-				}
-				_manager.ActorService.UpdateOneActor(actorViewModel);
-				return RedirectToAction(nameof(Index));
-			}
-			return View(actorViewModel);
-		}
-		public IActionResult Delete(int id)
-		{
-			var actor = _manager.ActorService.GetOneActor(id, false);
-			if (actor == null)
-			{
-				return NotFound();
-			}
-
-			return View(actor);
-		}
-
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public IActionResult DeleteConfirmed(int id)
-		{
-			var actor = _manager.ActorService.GetOneActor(id, false);
-			if (actor != null)
-			{
-				_manager.ActorService.DeleteOneActor(id);
-			}
-			return RedirectToAction(nameof(Index));
 		}
 	}
 }
