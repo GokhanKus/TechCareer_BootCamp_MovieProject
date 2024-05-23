879a651f409fbd1c9a437b40fd61f37e7a80cdcd
Gokhan Kus <gkus1998@hotmail.com>
2024-05-23 12:53:13 +0300

    area(mini mvc) for admin added (controllers and views)

---
+++ TechCareer_BootCamp_MovieProject_UI/Areas/Admin/Controllers/GenresController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareer_BootCamp_MovieProject_UI.Areas.Admin.Controllers
+{
+	[Area("Admin")]//bu attributeyi yazarak bu controllerin Areas/Admin icerisinde bulundugunu belirtiriz
+				   //Aynı controller isminden oldugu icin Area attribute'sini eklemek zorundayız.
+	public class GenresController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ TechCareer_BootCamp_MovieProject_UI/Areas/Admin/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareer_BootCamp_MovieProject_UI.Areas.Admin.Controllers
+{
+	[Area("Admin")]//bu attributeyi yazarak bu controllerin Areas/Admin icerisinde bulundugunu belirtiriz
+				   //ayrica ayni isimdeki controller cakismalarini da onler ve farklı route'u tanimlar (Admin/Movies/Index..)
+	public class MoviesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
